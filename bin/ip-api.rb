8fbec142577e003b79b4663b4758dd00e1b0b02e
kk <kevin197011@outlook.com>
2024-12-26 06:51:23 +0000

    Update 2024-12-26 06:51:23 +0000.

--- bin/ip-api.rb
+++ bin/ip-api.rb
@@ -3,20 +3,25 @@
 
 require 'http'
 require 'json'
+require 'logger'
 
 # Class for interacting with the Ip-Api service to retrieve geolocation data based on an IP address.
 class IpApi
   attr_accessor :ip
 
   # Initialize with the given IP address.
-  def initialize(ip)
+  def initialize(ip, logger)
     @ip = ip
+    @logger = logger
   end
 
   # Run the process to fetch and display geolocation data for the provided IP.
   def run
     # Validate the IP address format before making the request.
-    exit 'Not a valid IP address!' unless valid_ip?(@ip)
+    unless valid_ip?(@ip)
+      @logger.error('Not a valid IP address!')
+      exit 1
+    end
 
     url = "http://ip-api.com/json/#{@ip}"
 
@@ -28,13 +33,13 @@ class IpApi
       if response.status.success?
         # Pretty print the JSON response for easier readability.
         json_body = JSON.pretty_generate(response.parse(:json))
-        Rails.logger.debug(json_body)
+        @logger.debug(json_body)
       else
-        Rails.logger.debug { "Failed to retrieve data: #{response.status}" }
+        @logger.error("Failed to retrieve data: #{response.status}")
       end
     rescue HTTP::Error => e
       # Log the error if an HTTP error occurs.
-      Rails.logger.debug { "An error occurred: #{e.message}" }
+      @logger.error("An error occurred: #{e.message}")
     end
   end
 
@@ -48,12 +53,16 @@ end
 
 # Execute the script if it's invoked directly from the command line.
 if __FILE__ == $PROGRAM_NAME
+  # Create a new Logger instance.
+  logger = Logger.new($stdout)
+  logger.level = Logger::DEBUG # Set the logging level to DEBUG for detailed logs.
+
   # Ensure that exactly one argument (IP address) is provided.
   if ARGV.length != 1
-    puts "Usage: ruby #{$PROGRAM_NAME} <IP_ADDRESS>"
+    logger.error("Usage: ruby #{$PROGRAM_NAME} <IP_ADDRESS>")
     exit 1
   end
 
   # Instantiate and run the IpApi class with the provided IP address.
-  IpApi.new(ARGV[0]).run
+  IpApi.new(ARGV[0], logger).run
 end
