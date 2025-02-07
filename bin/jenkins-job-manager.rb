0df414d722935a3b3de2ad11b813bf2ed5d96311
kk <kevin197011@outlook.com>
2025-02-07 09:29:41 +0000

    Update 2025-02-07 09:29:41 +0000.

--- bin/jenkins-job-manager.rb
+++ bin/jenkins-job-manager.rb
@@ -16,7 +16,6 @@ class JenkinsJobManager
     @auth_header = "Basic #{Base64.strict_encode64("#{@user}:#{@password}")}"
   end
 
-  # 导出所有作业
   def export_all_jobs
     FileUtils.mkdir_p('jobs')
 
@@ -30,7 +29,6 @@ class JenkinsJobManager
     end
   end
 
-  # 从文件导入或更新指定的作业
   def import_job_from_file(job_name)
     file_path = "jobs/#{job_name}.xml"
     if File.exist?(file_path)
@@ -41,7 +39,6 @@ class JenkinsJobManager
     end
   end
 
-  # 从jobs目录导入或更新所有作业
   def import_all_jobs_from_files
     job_files = Dir.glob('jobs/*.xml')
     job_files.each do |file_path|
@@ -52,7 +49,6 @@ class JenkinsJobManager
     end
   end
 
-  # 获取所有作业的名称
   def get_all_job_names
     url = "#{@jenkins_url}/api/json?tree=jobs[name]"
     response = HTTP.get(url, headers: { 'Authorization' => @auth_header })
@@ -68,7 +64,6 @@ class JenkinsJobManager
     []
   end
 
-  # 导出指定作业的配置
   def export_job(job_name)
     url = "#{@jenkins_url}/job/#{job_name}/config.xml"
     response = HTTP.get(url, headers: { 'Authorization' => @auth_header })
@@ -81,31 +76,23 @@ class JenkinsJobManager
     nil
   end
 
-  # 导入或更新作业
   def import_or_update_job(job_name, config_xml)
     url = "#{@jenkins_url}/job/#{job_name}/config.xml"
+    puts url
     begin
-      response = HTTP.get(url, headers: { 'Authorization' => @auth_header })
-      if response.status.success?
-        puts "Updating existing job #{job_name}"
-        HTTP.put(url, body: config_xml, headers: {
-                   'Authorization' => @auth_header,
-                   'Content-Type' => 'application/xml'
-                 })
-      end
-    rescue HTTP::ConnectionError, HTTP::ResponseError => e
-      if e.message.include?('404')
-        puts "Creating new job #{job_name}"
-        create_new_job(job_name, config_xml)
-      else
-        puts "Error updating job #{job_name}: #{e.message}"
-      end
+      puts "Creating new job #{job_name}"
+      create_new_job(job_name, config_xml)
+    rescue Exception
+      puts "Updating existing job #{job_name}"
+      HTTP.put(url, body: config_xml, headers: {
+                 'Authorization' => @auth_header,
+                 'Content-Type' => 'application/xml'
+               })
     end
   end
 
   private
 
-  # 创建新的作业
   def create_new_job(job_name, config_xml)
     url = "#{@jenkins_url}/createItem?name=#{job_name}"
     response = HTTP.post(url, body: config_xml, headers: {
@@ -120,11 +107,9 @@ class JenkinsJobManager
   end
 end
 
-# 使用示例
 jenkins_url = 'https://jenkins.devops.io'
 user = 'kk'
-password = 'xxxxxxxxxxxxxxxxxxxxx' # Jenkins API Token Or password
+password = 'xxxxxxxxxxxxxxxxxxxxxxxx' # Jenkins API Token Or password
 manager = JenkinsJobManager.new(jenkins_url, user, password)
 
-# 从jobs目录下导入或更新所有作业
 manager.export_all_jobs
