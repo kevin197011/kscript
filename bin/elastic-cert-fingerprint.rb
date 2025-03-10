bd9026fb8e4b19070886e243409dfd64d88c05e8
kk <61411931+kevin197011@users.noreply.github.com>
2025-03-10 17:32:49 +0800

    Create elastic-cert-fingerprint.rb

---
+++ bin/elastic-cert-fingerprint.rb
@@ -0,0 +1,9 @@
+require 'openssl'
+
+cert_path = 'elasticsearch.crt'
+cert = OpenSSL::X509::Certificate.new(File.read(cert_path))
+
+fingerprint = OpenSSL::Digest::SHA256.hexdigest(cert.to_der)
+formatted_fingerprint = fingerprint.scan(/../).join(':').upcase
+
+puts formatted_fingerprint
